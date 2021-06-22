729f2a70b77471b4e78c4527862f068cc3bcc84d
grvxs <gauravsinghsisodia5@gmail.com>
2021-06-22 15:15:36 +0530

    Fix spotify script

--- xmonad/.config/xmobar/scripts/spotify.sh
+++ xmonad/.config/xmobar/scripts/spotify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
-            org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' \
+            org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' 2>&1\
             string:'Metadata' |\
-            awk -F '"' 'BEGIN {RS=" entry"}; /"xesam:artist"/ {a = $4} /"xesam:album"/ {b = $4}/"xesam:title"/ {t = $4} END {print a " - " t}'
+            awk -F '"' 'BEGIN {RS=" entry"}; /"xesam:artist"/ {a = $4} /"xesam:album"/ {b = $4}/"xesam:title"/ {t = $4} END {print a "\n" t}'|\
+            head -c -1 |\
+            awk -v d=" - " '{s=(NR==1?s:s d)$0}END{print s}'
