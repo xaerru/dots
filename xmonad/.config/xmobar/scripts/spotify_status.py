e561f69923ae7cb34df54e2f1aaa4fbcd0e84e6e
grvxs <gauravsinghsisodia5@gmail.com>
2021-06-22 13:24:25 +0530

    Remove spotify status script

--- xmonad/.config/xmobar/scripts/spotify_status.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python3
-import subprocess
-script = """
-spotifyid=$(ps -ef | grep -e '[/]usr/share/spotify' -e '[/]opt/spotify' | awk '{print $2}' | head -1)
-wmctrl -l -p | grep $spotifyid | sed -n 's/.*'$HOSTNAME'//p'
-"""
-output = subprocess.check_output(script, shell=True).decode('UTF-8').strip()
-if output!="Spotify Free" and output !="Spotify Premium" and output!="Advertisement":
-    print(output)
