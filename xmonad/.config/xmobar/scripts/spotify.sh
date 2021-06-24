75e27871658ef988dd941fc5cd07bf93db22b0f5
grvxs <gauravsinghsisodia5@gmail.com>
2021-06-24 19:44:09 +0530

    New spotify script

--- xmonad/.config/xmobar/scripts/spotify.sh
+++ xmonad/.config/xmobar/scripts/spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 \
-            org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' 2>&1\
-            string:'Metadata' |\
-            awk -F '"' 'BEGIN {RS=" entry"}; /"xesam:artist"/ {a = $4} /"xesam:album"/ {b = $4}/"xesam:title"/ {t = $4} END {print a "\n" t}'|\
-            head -c -1 |\
-            awk -v d=" - " '{s=(NR==1?s:s d)$0}END{print s}'
+running=$(pidof spotify)
+if [ "$running" != "" ]; then
+    artist=$(playerctl metadata artist)
+    song=$(playerctl metadata title | cut -c 1-60)
+    echo -n "$artist · $song"
+fi
