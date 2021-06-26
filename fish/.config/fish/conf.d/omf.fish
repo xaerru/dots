99bffc7bc31677760de1cdb7e0cbea94376da40e
grvxs <gauravsinghsisodia5@gmail.com>
2021-06-26 21:51:43 +0530

    Add nnn support

--- fish/.config/fish/conf.d/omf.fish
+++ fish/.config/fish/conf.d/omf.fish
@@ -14,10 +14,12 @@ set -gx PATH "$HOME/.cabal/bin" $PATH;
 
 # Exports
 export QT_QPA_PLATFORMTHEME=qt5ct
-export EDITOR=nvim
+export EDITOR='nvim'
 export MANPAGER="nvim -c 'set ft=man' -"
 export SHELL=/usr/bin/fish
 export TERM=xterm-256color
+export NNN_PLUG='o:fzopen;v:imgview;s:preview-tui;'
+set --export NNN_FIFO "/tmp/nnn.fifo"
 
 # Alias
 alias v="nvim"
