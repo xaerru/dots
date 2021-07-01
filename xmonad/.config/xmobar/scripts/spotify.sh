b44973ad7ed543bd6f58a139754f443006e3b9ca
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-01 06:05:39 +0530

    New song script

--- xmonad/.config/xmobar/scripts/spotify.sh
+++ xmonad/.config/xmobar/scripts/spotify.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-running=$(pidof spotify)
-if [ "$running" != "" ]; then
-    artist=$(playerctl metadata artist)
-    song=$(playerctl metadata title | cut -c 1-60)
-    echo -n "$artist · $song"
-fi
+playerctl metadata --format "{{ artist }} - {{ album }} - {{ title }}"
