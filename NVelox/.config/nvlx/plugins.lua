979a74713b2635896c483a029f2d043b64688110
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-10 09:46:35 +0530

    feat(nvlx): add discord presence

--- NVelox/.config/nvlx/plugins.lua
+++ NVelox/.config/nvlx/plugins.lua
@@ -6,6 +6,28 @@ return {
     },
 
     {
+        "andweeb/presence.nvim",
+        config = function()
+            require("presence"):setup({
+                auto_update = true,
+                neovim_image_text = "The One True Text Editor",
+                main_image = "file",
+                log_level = nil,
+                debounce_timeout = 10,
+                enable_line_number = false,
+                blacklist = {},
+                editing_text = "Editing %s",
+                file_explorer_text = "Browsing %s",
+                git_commit_text = "Committing changes",
+                plugin_manager_text = "Managing plugins",
+                reading_text = "Reading %s",
+                workspace_text = "Working on %s",
+                line_number_text = "Line %s out of %s", -- Line number format string (for when enable_line_number is set to true)
+            })
+        end,
+    },
+
+    {
         "phaazon/hop.nvim",
         after = "nordbuddy",
     },
