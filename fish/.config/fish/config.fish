54eb6ef145d7c7d14559c106b7ae2b696d551ada
grvxs <gauravsinghsisodia5@gmail.com>
2021-08-24 19:27:50 +0530

    remove(fish): insert mode binds

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,53 +1,53 @@
 set fish_greeting
 
-bind -M insert -m default jk backward-char force-repaint
-bind -M insert -m default kj backward-char force-repaint
+#bind -M insert -m default jk backward-char force-repaint
+#bind -M insert -m default kj backward-char force-repaint
 
 fish_vi_key_bindings
 
 function fish_mode_prompt
-  switch $fish_bind_mode
-    case default
-      echo -en "\e[2 q"
-      tput bold
-      tput setab 1
-      tput setaf 7
-      echo "[N]"
-      tput sgr0
-      echo " "
-    case insert
-      echo -en "\e[6 q"
-      tput bold
-      tput setab 2
-      tput setaf 7
-      echo "[I]"
-      tput sgr0
-      echo " "
-    case replace_one
-      echo -en "\e[4 q"
-      tput bold
-      tput setab 6
-      tput setaf 7
-      echo "[R]"
-      tput sgr0
-      echo " "
-    case visual
-      echo -en "\e[2 q"
-      tput bold
-      tput setab 5
-      tput setaf 7
-      echo "[V]"
-      tput sgr0
-      echo " "
-    case '*'
-      echo -en "\e[2 q"
-      tput bold
-      tput setab 2
-      tput setaf 7
-      echo "[?]"
-      tput sgr0
-      echo " "
-  end
+    switch $fish_bind_mode
+        case default
+            echo -en "\e[2 q"
+            tput bold
+            tput setab 1
+            tput setaf 7
+            echo "[N]"
+            tput sgr0
+            echo " "
+        case insert
+            echo -en "\e[6 q"
+            tput bold
+            tput setab 2
+            tput setaf 7
+            echo "[I]"
+            tput sgr0
+            echo " "
+        case replace_one
+            echo -en "\e[4 q"
+            tput bold
+            tput setab 6
+            tput setaf 7
+            echo "[R]"
+            tput sgr0
+            echo " "
+        case visual
+            echo -en "\e[2 q"
+            tput bold
+            tput setab 5
+            tput setaf 7
+            echo "[V]"
+            tput sgr0
+            echo " "
+        case '*'
+            echo -en "\e[2 q"
+            tput bold
+            tput setab 2
+            tput setaf 7
+            echo "[?]"
+            tput sgr0
+            echo " "
+    end
 end
 
 if status is-login
