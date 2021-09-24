4750c34588a5935dc25b229db692ba1a2d8ef196
xaerru <xaerru@gmail.com>
2021-09-24 10:30:36 +0530

    feat(nvlx): new way of config

--- NVelox/.config/nvlx/init.lua
+++ NVelox/.config/nvlx/init.lua
@@ -1,24 +1,25 @@
-local plugins = require("plugins")
+local nvlx = require("_nvlx.empty_config")
 
 nvlx.general = {
     colorscheme = "nordbuddy",
     autosave = true,
     winblend = 25,
-    highlights = {
-        TSNodeKey = {
-            cterm = "bold",
-            ctermfg = 198,
-            gui = "bold",
-            guifg = "#8fbcbb",
-        },
-        TSNodeUnmatched = {
-            ctermfg = 242,
-            guifg = "#4c566a",
-        },
+}
+
+nvlx.highlights = {
+    TSNodeKey = {
+        cterm = "bold",
+        ctermfg = 198,
+        gui = "bold",
+        guifg = "#8fbcbb",
+    },
+    TSNodeUnmatched = {
+        ctermfg = 242,
+        guifg = "#4c566a",
     },
 }
 
-nvlx.keybinds.leader = {
+nvlx.maps.leader = {
     normal = {
         H = {
             {
@@ -65,7 +66,7 @@ nvlx.keybinds.leader = {
     },
 }
 
-nvlx.keybinds.general = {
+nvlx.maps.general = {
     insert = {
         ["jj"] = "<Right>",
         ["<C-j>"] = "<ESC>:m .+1<CR>==i",
@@ -90,14 +91,12 @@ nvlx.keybinds.general = {
         ["K"] = ":m '<-2<CR>gv=gv",
     },
     command = {
-        ["w!!"] = "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!"
-    }
+        ["w!!"] = "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!",
+    },
 }
 
-nvlx.plugins = plugins
-
 nvlx.config.plugins.gitsigns = {
-    word_diff = true
+    word_diff = true,
 }
 
 nvlx.config.plugins.autopairs = {
@@ -107,7 +106,7 @@ nvlx.config.plugins.autopairs = {
     },
 }
 
-nvlx.settings = {
+nvlx.options = {
     iskeyword = vim.opt.iskeyword + { "-" } - { "_" },
     list = true,
     colorcolumn = "100",
@@ -151,3 +150,87 @@ nvlx.disabled = {
         "matchit",
     },
 }
+
+nvlx.plugins = {
+    {
+        "p00f/cphelper.nvim",
+        rocks = "http",
+        after = "nordbuddy",
+    },
+
+    {
+        "neovimhaskell/haskell-vim",
+        event = "BufRead",
+        ft = "haskell",
+    },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        config = function()
+            vim.g.mkdp_auto_start = 1
+        end,
+        ft = "markdown",
+    },
+
+    {
+        "chaoren/vim-wordmotion",
+    },
+
+    {
+        "andweeb/presence.nvim",
+        config = function()
+            require("presence"):setup({
+                auto_update = true,
+                neovim_image_text = "The One True Text Editor",
+                main_image = "file",
+                log_level = nil,
+                debounce_timeout = 10,
+                enable_line_number = false,
+                blacklist = {},
+                editing_text = "Editing %s",
+                file_explorer_text = "Browsing %s",
+                git_commit_text = "Committing changes",
+                plugin_manager_text = "Managing plugins",
+                reading_text = "Reading %s",
+                workspace_text = "Working on %s",
+                line_number_text = "Line %s out of %s", -- Line number format string (for when enable_line_number is set to true)
+            })
+        end,
+        after = "nordbuddy",
+    },
+
+    {
+        "phaazon/hop.nvim",
+        after = "nordbuddy",
+    },
+
+    {
+        "tweekmonster/startuptime.vim",
+        cmd = "StartupTime",
+    },
+
+    {
+        "mfussenegger/nvim-ts-hint-textobject",
+        config = function()
+            require("tsht").config.hint_keys = { "a", "o", "e", "i", "d", "h", "t", "n" }
+        end,
+        after = "nvim-treesitter",
+    },
+
+    {
+        "mizlan/iswap.nvim",
+        config = function()
+            require("iswap").setup({
+                keys = "aoeuidhtn",
+            })
+        end,
+        after = "nvim-treesitter",
+    },
+
+    {
+        "JoseConseco/vim-case-change",
+        after = "nordbuddy",
+    },
+}
+
+return nvlx
