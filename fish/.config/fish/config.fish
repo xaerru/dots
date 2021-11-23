449fa1999e4d4f59a9e8970d95284a03d2548b9a
xaerru <xaerru@gmail.com>
2021-11-23 11:15:55 +0530

    feat(fish): new user

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -50,4 +50,4 @@ function fish_mode_prompt
     end
 end
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/grvxs/.ghcup/bin # ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/xaerru/.ghcup/bin # ghcup-env
